a93268cd194184879444073b0bb693aa88761f61
Alex Codreanu <126508471+AlexCodreanu23@users.noreply.github.com>
2024-03-05 17:01:39 +0200

    Update Student.java

--- lab1/src/lab1/task2/Student.java
+++ lab1/src/lab1/task2/Student.java
@@ -4,6 +4,10 @@ public class Student {
     private String name;
     private double grade;
 
+    public Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
     public String getName(){
         return name;
     }
